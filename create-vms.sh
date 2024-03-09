c27807e86ffaab933e14d997d8ed39872314816d
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2024-03-09 22:25:58 +0100

    install works

--- create-vms.sh
+++ create-vms.sh
@@ -2,19 +2,30 @@
 
 set -eu
 
+DIR="$(dirname "$(realpath "$0")")"
+
 NAME="$1"
 
 mkdir -p tmp
 mkdir -p vm-state
 
-IMG=debian-12-genericcloud-amd64.qcow2
+# https://mop.koeln/blog/creating-a-local-debian-vm-using-cloud-init-and-libvirt/
+# > DO NOT DOWNLOAD THE GENERICCLOUD IMAGE
+IMG=debian-12-generic-amd64.qcow2
 
 if ! [ -f "tmp/$IMG" ]; then
-    curl -L -o "tmp/$IMG" https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-genericcloud-amd64.qcow2
+    curl -L -o "tmp/$IMG" https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-generic-amd64.qcow2
 fi
 
 DISK="vm-state/$NAME.qcow2"
 cp "tmp/$IMG" "$DISK"
+
+meta_data=$(mktemp)
+
+cat >"$meta_data" <<EOF
+instance-id: $NAME
+local-hostname: $NAME
+EOF
 
 virt-install -n "$NAME" \
     --os-variant=debian12 \
@@ -22,5 +33,11 @@ virt-install -n "$NAME" \
     --import --disk "path=$DISK,bus=virtio" \
     --network network=default,model=virtio \
     --graphics=none --rng /dev/urandom \
-    --cloud-init=user-data=user-data
-    #--noautoconsole
+    "--cloud-init=user-data=$DIR/user-data,meta-data=$meta_data" \
+    --noautoconsole
+
+rm "$meta_data"
+
+echo "Successfully created $NAME"
+
+virsh domifaddr "$NAME"
